aaae28d28becb8579d91b5340c29a4f7adde2269
Meagan Chase <meaganchase814@yahoo.com>
2018-10-02 16:52:51 -0400

    fixed title

--- app/controllers/trello_controller.rb
+++ app/controllers/trello_controller.rb
@@ -2,14 +2,20 @@ class TrelloController < ApplicationController
   
   def index
     @trello = Posit.new
-    @trello.body = params["notes"] 
+    @trello.title = params["title"] 
+    @trello.body = params["body"] 
     @trello.save
     redirect_to trello_sticky_url
   end
   def sticky
     @trello = Posit.all
   end
-
+  #def destroy
+   # @trello = Posit.find(params[:id])
+    #@trello.destroy
+ 
+    #redirect_to trello_sticky_url
+  #end
 
   
 end
